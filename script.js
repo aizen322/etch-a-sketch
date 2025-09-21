fe59954abd90cae8e9f798e52a4ec3e45f0e4c85
jv <147636587+aizen322@users.noreply.github.com>
2025-09-22 01:17:44 +0800

    can statically draw black pen as of now, also added clear sketch function working. currently working on eraser tool

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const container = document.querySelector("#grid-container");
 
+const staticBtn = document.getElementById("staticMode");
+const rainbowBtn = document.getElementById("rainbowMode");
+const eraserBtn = document.getElementById("eraser");
+const clearBtn = document.getElementById("clearSketch");
+const toggleBtn = document.getElementById("toggleGrid");
+
+
 let size = 16;
+let isDrawing = false;
+let action = "draw";
 
 function createGrid(){
     container.innerHTML = '';
@@ -15,8 +24,59 @@ function createGrid(){
         }
         container.appendChild(row);
     }
+
 }
 
-createGrid();
+function draw(grid) {
+    grid.style.backgroundColor = 'black'; 
+}
 
+function erase(grid){
+    grid.style.backgroundColor = 'white';
+}
+
+
+createGrid();
 const gridItems = document.querySelectorAll('.grid');
+
+gridItems.forEach(grid =>{
+    console.log(action);
+    if(action === "draw"){
+        grid.addEventListener("mousedown", () =>{
+            isDrawing = true;
+            draw(grid);
+        });
+        grid.addEventListener("mouseover", () =>{
+            if(isDrawing)
+            draw(grid);
+        });
+        grid.addEventListener("mouseup", () =>{
+            isDrawing = false;
+        });
+    }
+    else if(action === "erase"){
+         grid.addEventListener("mousedown", () =>{
+            isDrawing = true;
+            erase(grid);
+        });
+        grid.addEventListener("mouseover", () =>{
+            if(isDrawing)
+            erase(grid);
+        });
+        grid.addEventListener("mouseup", () =>{
+            isDrawing = false;
+        });
+    }
+    
+});
+
+
+//////////////////////////////
+clearBtn.onclick = () => clearSketch();
+function clearSketch(){
+    gridItems.forEach(grid =>{
+        grid.style.backgroundColor = 'white';
+    });
+}
+
+
